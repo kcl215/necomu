071e21b6cef8c4e63d207db1d496cf9925190602
kcl215 <kcl988780@gmail.com>
2022-04-09 08:47:11 +0000

    [fix]

--- config/routes.rb
+++ config/routes.rb
@@ -9,19 +9,16 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
   }
 
-  # 会員側
   root to: "public/homes#top"
-  # get "customers/my_page" => "public/customers#show"
-  # get "public/customers#show"
-
-
 
   scope module: :public do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
     end
+    
     resources :cats, only: [:show, :new, :create, :edit, :update]
+    
     resources :customers, only: [:show, :edit, :update] do
       member do
         get :favorites
@@ -36,27 +33,5 @@ Rails.application.routes.draw do
     resources :customers, only: [:index, :show, :edit, :update]
   end
 
-  # namespace :public do
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  # end
-  # namespace :public do
-  #   get 'homes/top'
-  # end
-  # namespace :admin do
-  #   get 'customers/index'
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  # end
-  # namespace :public do
-  #   get 'posts/index'
-  #   get 'posts/show'
-  #   get 'posts/new'
-  # end
-  # namespace :public do
-  #   get 'cats/show'
-  #   get 'cats/new'
-  #   get 'cats/edit'
-  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

d9e203a4421aef9a060aef42dbeb441fc71b9fb3
kcl215 <kcl988780@gmail.com>
2022-04-11 10:06:30 +0000

    [add]adminログイン機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:name]
 
   has_many :posts, dependent: :destroy
   has_many :cats, dependent: :destroy
@@ -32,5 +32,7 @@ class Customer < ApplicationRecord
   def following?(customer)
     followings.include?(customer)
   end
+
+
 
 end

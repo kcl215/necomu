b9120c85141868022e990b4290ce561935dbb3e5
kcl215 <kcl988780@gmail.com>
2022-04-17 01:56:20 +0000

    [add]ページネーションの追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,7 +1,7 @@
 class Public::SearchesController < ApplicationController
 
     def search
-    @customers = Customer.looks(params[:search], params[:word])
+    @customers = Customer.looks(params[:search], params[:word]).page(params[:page]).per(20)
     end
 
 end
